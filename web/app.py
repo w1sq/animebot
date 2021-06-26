4d0c9b2cecd7fe1b7e5c58f33cdcdb656df30d09
page <artem.kokorev2005@yandex.ru>
2021-06-27 00:33:31 +0300

    pochti

--- web/app.py
+++ web/app.py
@@ -23,4 +23,4 @@ def main(id):
     return render_template("main.html",link = link,title = title, views = anime.views,id=id,img_link=img_link,page_link = page_link)
 
 if __name__ == "__main__":
-    app.run()
+    app.run('0.0.0.0',port=5000)
