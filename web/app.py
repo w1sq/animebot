2609d4ab3e6f763ef62c3c108895ede83c7f44eb
page <artem.kokorev2005@yandex.ru>
2021-06-27 07:15:12 +0300

    added 404 error handler and waitress serve

--- web/app.py
+++ web/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
-import requests
 import logging
 from db_data import db_session
 from db_data.__all_models import Anime
+from waitress import serve
 
 app = Flask(__name__,template_folder='templates')
 app.config["SECRET_KEY"] = "ikbgfWnhHUHSDFNA8w83tyy32"
@@ -22,5 +22,10 @@ def main(id):
     page_link = f'https://bot.animepoint.cc/{id}'
     return render_template("main.html",link = link,title = title, views = anime.views,id=id,img_link=img_link,page_link = page_link)
 
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template("error404.html")
+
+
 if __name__ == "__main__":
-    app.run('0.0.0.0',port=5000)
+    serve(app,'0.0.0.0',port=5000)
